62b7051cf3ff6ff04507eb4306980e9be20fc49c
JuliaMathias <juliamss@gmail.com>
2023-06-16 14:18:52 -0300

    make credo happy

--- lib/college_tracker_web/components/core_components.ex
+++ lib/college_tracker_web/components/core_components.ex
@@ -16,8 +16,10 @@ defmodule CollegeTrackerWeb.CoreComponents do
   """
   use Phoenix.Component
 
-  alias Phoenix.LiveView.JS
   import CollegeTrackerWeb.Gettext
+
+  alias Phoenix.HTML.Form
+  alias Phoenix.LiveView.JS
 
   @doc """
   Renders a modal.
@@ -296,8 +298,7 @@ defmodule CollegeTrackerWeb.CoreComponents do
   end
 
   def input(%{type: "checkbox", value: value} = assigns) do
-    assigns =
-      assign_new(assigns, :checked, fn -> Phoenix.HTML.Form.normalize_value("checkbox", value) end)
+    assigns = assign_new(assigns, :checked, fn -> Form.normalize_value("checkbox", value) end)
 
     ~H"""
     <div phx-feedback-for={@name}>
